a4931bbb015f1550ab9b6f5e57cd9098cb011c13
sasumwen <imarhiagbeosasumwen@gmail.com>
2026-01-05 09:42:07 -0700

    Connecting to  Railway

--- app.py
+++ app.py
@@ -65,5 +65,8 @@ def toggle_sidebar(n, current_sidebar_width):
         return {"display": "block"}, 2, 10
     
 # Run the app
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True, port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port, debug=False)
